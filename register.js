9f5d0a5b7059bddef7fbf81fdd98087e51cecdf5
Sripal Rakkireddy <sripalrakkireddy1817@gmail.com>
2026-04-23 16:58:33 +0530

    New files

---
+++ register.js
@@ -0,0 +1,53 @@
+let url = "http://localhost:3000/tasks";
+
+let form = document.querySelector("form");
+form.addEventListener("submit", () => {
+  let formData = new FormData(form);
+
+  let password = formData.get("password");
+  let confirmPassword = formData.get("confirmPassword");
+  let name = formData.get("name");
+  let email = formData.get("email");
+  let mobileNo = formData.get("contact");
+  let gender = formData.get("gender");
+  let dob = formData.get("dob");
+  let address = formData.get("address");
+
+  if (
+    password === "" ||
+    confirmPassword === "" ||
+    name === "" ||
+    email === "" ||
+    address === "" ||
+    gender === "" ||
+    dob === ""
+  ) {
+    alert("Fill all the feilds");
+  } else if (password === confirmPassword) {
+    let userDetails = {
+      name: name,
+      email: email,
+      password: password,
+      dob: dob,
+      gender: gender,
+      contact: mobileNo,
+    };
+    addUser(userDetails);
+  } else {
+    alert("Password Mismatch");
+  }
+});
+
+let addUser = async (data) => {
+  try {
+    await fetch(url, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+  } catch {
+    alert("Something went wrong try after some time");
+  }
+};
